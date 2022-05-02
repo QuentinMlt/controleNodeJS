158fda2b19a3cc7c6dcf9c8d283f24638e44afdd
QuentinMlt <quentinmaillotpro@gmail.com>
2022-05-02 16:16:56 +0200

    ok : ajout d'un try/catch pour l'apparition de la page 500 erreur interne (Q5)

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/" && req.method == "GET") {
-        res.writeHead(200, {'content-type':'text/html'});
-        res.write('<h1>HELLO WORD Quentin</h1>');
-      }
-    else if (req.url === "/" && req.method != "GET") {
-        res.writeHead(200, {'content-type':'text/html'});
-        res.write('<h1>401 Méthode non authorisée</h1>');
-      } 
-    else {
-        res.writeHead(404, {'content-type':'text/html'});
-        res.write('<h1>404 Page introuvable</h1>');
+    try {
+        console.logggg();
+        if (req.url === "/" && req.method == "GET") {
+            res.writeHead(200, {'content-type':'text/html'});
+            res.write('<h1>HELLO WORD Quentin</h1>');
+        }
+        else if (req.url === "/" && req.method != "GET") {
+            res.writeHead(200, {'content-type':'text/html'});
+            res.write('<h1>401 Méthode non authorisée</h1>');
+        } 
+        else {
+            res.writeHead(404, {'content-type':'text/html'});
+            res.write('<h1>404 Page introuvable</h1>');
+        }
+    } catch (err) {
+        res.writeHead(500, {'content-type':'text/html'});
+        res.write('<h1>500 Erreur interne au serveur</h1>');
       }
 });
 server.listen(5000);
