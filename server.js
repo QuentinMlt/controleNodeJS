d45b6b8f8711b0f475d1d33a7339c077ed687207
QuentinMlt <quentinmaillotpro@gmail.com>
2022-05-02 16:20:07 +0200

    ok : Suppression erreur pour l'acces à la page 500 erreur interne

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     try {
-        console.logggg();
+        
         if (req.url === "/" && req.method == "GET") {
             res.writeHead(200, {'content-type':'text/html'});
             res.write('<h1>HELLO WORD Quentin</h1>');
