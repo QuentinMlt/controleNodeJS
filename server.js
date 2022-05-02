0a1263d181c3e746ecaa5334dae199fbb8f20365
QuentinMlt <quentinmaillotpro@gmail.com>
2022-05-02 16:01:39 +0200

    ok : retourne 401 si methode est different de Get sur la route / (Q3)

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const server = http.createServer((req, res) => {
     if (req.url === "/" && req.method == "GET") {
         res.writeHead(200, {'content-type':'text/html'});
         res.write('<h1>HELLO WORD Quentin</h1>');
+      }
+    else if (req.url === "/" && req.method != "GET") {
+        res.writeHead(200, {'content-type':'text/html'});
+        res.write('<h1>401 Méthode non authorisée</h1>');
       } 
 });
 server.listen(5000);
