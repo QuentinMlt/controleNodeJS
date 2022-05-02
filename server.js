ba884a895ed8bcc38b4308eea17dad78b76895ba
QuentinMlt <quentinmaillotpro@gmail.com>
2022-05-02 16:39:56 +0200

    ko : Correction erreur status code pour méthode non autorisée

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
             res.write('<h1>HELLO WORD Quentin</h1>');
         }
         else if (req.url === "/" && req.method != "GET") {
-            res.writeHead(200, {'content-type':'text/html'});
+            res.writeHead(401, {'content-type':'text/html'});
             res.write('<h1>401 Méthode non authorisée</h1>');
         } 
         else {
